00afa77540cb22cf1ccc46e1af81913f4f88b346
bar-usr <bernardorocha.bz@gmail.com>
2025-05-10 01:29:22 +0100

    style: update website background and card colors for improved appearance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 
 const CardListElement = ({image, title, description}: {image: string, description: string, title: string}) => {
   return (
-    <div className="border border-zinc-700 bg-zinc-900/30 inset-shadow-sm p-4 rounded-md flex flex-row items-center gap-5">
+    <div className="bg-gray-500/10 inset-shadow-sm p-4 rounded-xl flex flex-row items-center gap-5">
       <Image src={image} alt={title} width={50} height={50} />
       <div className="flex flex-col">
         <h3 className="text-lg font-bold text-gray-100">{title}</h3>
@@ -22,7 +22,7 @@ const Card = ({ title, description, elements }: {
   elements?: { image: string, title: string, description: string }[] 
 }) => {
   return (
-    <div className="border border-zinc-700 bg-zinc-950/20 pt-4 pb-7 px-5 rounded-md flex flex-col gap-2 items-center w-[450px] shadow-lg shadow-zinc-600/10 hover:shadow-xl transition-all duration-300 backdrop-blur-sm">
+    <div className="bg-gray-800/40 pt-4 pb-7 px-5 rounded-xl flex flex-col gap-2 items-center w-[450px] shadow-lg shadow-zinc-600/10 hover:shadow-xl transition-all duration-300 backdrop-blur-sm">
       <h3 className="text-[21px] font mb-2 text-gray-100 ">{title}</h3>
       {description && <p className="text-sm text-gray-400 text-center">{description}</p>}
       <div className="flex flex-col gap-2 w-full">
@@ -43,13 +43,13 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <div className="mt-60 mb-10 flex flex-col p-10 justify-between items-center">
+    <div className="mt-60 mb-5 flex flex-col p-10 justify-between items-center">
       <Image src="/litbook_logo_white.png" alt="Litbook" width={400} height={400} />
-      <h2 className="text-2xl mb-15 text-gray-500">
+      <h2 className="text-2xl mb-15 text-zinc-500">
         Light up your language learning.
       </h2>
 
-      <div className="flex flex-row gap-4 mb-20">
+      <div className="flex flex-row gap-4">
         <button onClick={() => router.push('/tool')} className="bg-orange-400 text-white hover:bg-orange-500 hover:scale-105 transition-all duration-400 hover:cursor-pointer px-4 py-2 rounded-md">
           Generate My First Workbook
         </button>

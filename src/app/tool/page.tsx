bb242cc53a70bada753d9d44a0b8de31f8311c32
bar-usr <bernardorocha.bz@gmail.com>
2025-05-10 15:58:56 +0100

    feat: Enhance language selection functionality with improved button interactions and state management

--- src/app/tool/page.tsx
+++ src/app/tool/page.tsx
@@ -2,25 +2,84 @@
 
 import { useState } from "react";
 
-const LanguageCardElement = ({language, image, showText}: {language: string, image: string, showText: boolean}) => {
-  const [clicked, setIsClicked] = useState(false);
-
+const LanguageCardElement = ({
+  language, 
+  image, 
+  showText,
+  onClick,
+  isSelected
+}: {
+  language: string; 
+  image: string; 
+  showText: boolean; 
+  onClick?: () => void;
+  isSelected?: boolean;
+}) => {
   return(
-    <button onClick={() => setIsClicked(!clicked)} className={`flex flex-col gap-5 items-center ${clicked ? "bg-orange-900/50 hover:bg-orange-900/90" : "bg-gray-800/60 hover:bg-gray-700/60"} rounded-lg p-4 w-[120px] h-[120px] hover:cursor-pointer`}>
+    <button 
+    onClick={onClick} 
+    className={`flex flex-col gap-5 items-center ${
+      isSelected 
+        ? "bg-orange-900/50 hover:bg-orange-900/90" 
+        : "bg-gray-800/60 hover:bg-gray-700/60"
+      } rounded-lg p-4 w-[120px] h-[120px] hover:cursor-pointer`}
+    >
       <img src={image} alt={language} className="w-full h-full object-cover rounded-lg" />
       {showText && <h2 className="text-md font-bold">{language}</h2>}
     </button>
   )
 }
 
-const LanguageCard = ({ elements }: { elements: { language: string, image: string }[] }) => {
+const LanguageSelectButton = ({
+  image,
+  language,
+  handleClick,
+  isOpen
+}: {
+  image: string;
+  language: string;
+  handleClick: () => void;
+  isOpen: boolean;
+}) => {
+  return(
+    <button 
+    onClick={handleClick} 
+    className={`flex flex-col gap-5 items-center ${
+      isOpen 
+        ? "bg-orange-900/50 hover:bg-orange-900/90" 
+        : "bg-gray-800/60 hover:bg-gray-700/60"
+      } rounded-lg p-4 w-[120px] h-[120px] hover:cursor-pointer`}
+    >
+      <img src={image} alt={language} className="w-full h-full object-cover rounded-lg" />
+      <h2 className="text-md font-bold">{language}</h2>
+    </button>
+  )
+}
+
+const LanguageCard = ({ 
+  elements, 
+  handleSelect, 
+  selectedLanguage, 
+}: { 
+  elements: { language: string, image: string }[],  
+  handleSelect: (language: string) => void;
+  selectedLanguage: string;
+
+}) => {
   return(
     <div className="flex flex-row justify-center items-center mt-20"> 
       <div className="flex flex-col w-[550px] bg-gray-800/40 rounded-lg p-5">
         <h2 className="text-2xl font-bold mb-5 text-center">Choose a language</h2>
         <div className="border border-gray-300 rounded-lg flex flex-wrap justify-center items-center gap-8 p-5">
           {elements.map((element) => (
-            <LanguageCardElement key={element.language} language={element.language} image={element.image} showText={true} />
+            <LanguageCardElement 
+              key={element.language} 
+              language={element.language} 
+              image={element.image} 
+              showText={true} 
+              onClick={() => handleSelect(element.language)}
+              isSelected={selectedLanguage === element.language}
+            />
             ))}
         </div>
       </div>
@@ -46,14 +105,26 @@ export default function Tool() {
     {language: "Turkish", image: "images/country_flags/turkey.png"},
   ]
 
+  const [selectedLanguage, setSelectedLanguage] = useState(elements[0]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelect = (language: string) => {
+    const selected = elements.find(el => el.language === language);
+    if (selected) {
+      setSelectedLanguage(selected);
+    }
+  };
+
   return (
     <div>
       <div className="mt-10 flex flex-col justify-center items-center">
-        <div className="flex flex-row justify-center items-center gap-10 border border-gray-700 rounded-lg p-5">
-          <h1 className="text-2xl font-bold"> Selected language</h1>
-          <LanguageCardElement language={elements[0].language} image={elements[0].image} showText={false}/>
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-row justify-center items-center gap-10 border border-gray-700 rounded-lg p-5">
+            <h1 className="text-2xl font-bold"> Selected language</h1>
+            <LanguageSelectButton image={selectedLanguage.image} language={selectedLanguage.language} handleClick={() => setIsOpen(!isOpen)} isOpen={isOpen}/>
+          </div>
+          {isOpen && <LanguageCard elements={elements} handleSelect={handleSelect} selectedLanguage={selectedLanguage.language}/>}
         </div>
-        <LanguageCard elements={elements}/>
       </div>
     </div>
   )
